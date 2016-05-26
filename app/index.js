a4508f63ea155b920088c038b9be6a39258da79e
Matt <mvaden08@gmail.com>
2016-05-26 01:04:13 -0400

    Few more minor changes.

--- app/index.js
+++ app/index.js
@@ -1,48 +1,45 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-// console.log("Hello World")
-
-
 var post = {
-  title: "A blog post in React",
-  author: "Matthew Vaden",
-  body: "Welcome to my blog! This is a sample blog designed to show how make an app with ReactJS. I hope that you find it interesting!",
+  title: "Baby's First Post",
+  author: "The Baby",
+  body: "Look at me, I'm posting!",
   comments: [
-    "Gnarly!!!",
-    "No, thanks.",
-    "I appreciate your feedback.",
-    "I'm just here to troll."
+    "First!",
+    "Second!",
+    "Wait, babies can't blog..."
   ]
 }
 
-var Comment = React.createClass({
-  render: function(){
-    return(
-      <p>Comments: {this.props.commentBody}</p>
-    )
-  }
-});
-
-// props = properties
 var Post = React.createClass({
   render: function(){
-    var commentsList = this.props.comments.map(function(comment, index){ // Gets properties from comments and maps them. Passes each comment, and
-      return < Comment key = {index} commentBody = {comment} />
-  });
+    var commentsList = this.props.comments.map(function(comment, index) {
+      return <Comment key={index} commentBody={comment} />
+    });
 
     return (
       <div>
         <h1>{this.props.title}</h1>
         <p>By: {this.props.author}</p>
-        <h3>Comments</h3>
-        {commentList}
+        <p>{this.props.body}</p>
+
+        <h2>Comments</h2>
+        {commentsList}
       </div>
     )
   }
 });
 
+var Comment = React.createClass({
+  render: function(){
+    return (
+      <p>{this.props.commentBody}</p>
+    )
+  }
+})
+
 ReactDOM.render(
-  <Post title={post.title} author={post.author} body={post.body} comments={post.comments} />,
+  <Post title={post.title} author={post.author} body={post.body} comments={post.comments}/>,
   document.getElementById("app")
-)
+);

ac2ba3e555655a7a27ec338b2dc4e3177c6e39f9
Matt <mvaden08@gmail.com>
2016-05-26 09:00:25 -0400

    Attempting to add state for editing. Unsuccessful.

--- app/index.js
+++ app/index.js
@@ -2,44 +2,60 @@ var React = require("react");
 var ReactDOM = require("react-dom");
 
 var post = {
-  title: "Baby's First Post",
-  author: "The Baby",
-  body: "Look at me, I'm posting!",
+  title: "A blog post in React",
+  author: "Matthew Vaden",
+  body: "Welcome to my blog! This is a sample blog designed to show how make an app with ReactJS. I hope that you find it interesting!",
   comments: [
-    "First!",
-    "Second!",
-    "Wait, babies can't blog..."
+    "Gnarly!!!",
+    "No, thanks.",
+    "I appreciate your feedback.",
+    "I'm just here to troll."
   ]
 }
 
-var Post = React.createClass({
+var Comment = React.createClass({
   render: function(){
-    var commentsList = this.props.comments.map(function(comment, index) {
-      return <Comment key={index} commentBody={comment} />
+    return(
+      <p>Comments:  {this.props.commentBody}</p>
+    )
+  }
+});
+
+// props = properties
+var Post = React.createClass({
+  get initialState: function(){
+    return
+      body: post.body
+    }
+  },
+  editPost function(){
+    newBody = prompt("Enter or edit your post here:");
+    this.setState({
+      body: newBody
     });
+  },
+
+  render: function(){
+    var commentsList = this.props.comments.map(function(comment, index){ // Gets properties from comments and maps them. Passes each comment, and
+      return < Comment key = {index} commentBody = {comment} />
+  });
 
     return (
       <div>
         <h1>{this.props.title}</h1>
         <p>By: {this.props.author}</p>
-        <p>{this.props.body}</p>
+        <p> {this.state.body}</p>
 
-        <h2>Comments</h2>
+        <button onClick=(this.editPost)>Edit Post</button>
+
+        <h3>Comments</h3>
         {commentsList}
       </div>
     )
   }
 });
 
-var Comment = React.createClass({
-  render: function(){
-    return (
-      <p>{this.props.commentBody}</p>
-    )
-  }
-})
-
 ReactDOM.render(
-  <Post title={post.title} author={post.author} body={post.body} comments={post.comments}/>,
+  <Post title={post.title} author={post.author} body={post.body} comments={post.comments} />,
   document.getElementById("app")
-);
+)
